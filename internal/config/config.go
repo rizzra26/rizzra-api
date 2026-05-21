871a706fcc5598656b819c2bf78d041baf4fcf5f
rizzra26 <rizukirimu@gmail.com>
2026-05-21 21:04:53 +0700

    fix: read dotenv file for env variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,25 +4,28 @@ import (
 	"fmt"
 
 	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	DBHost     string `env:"DB_HOST" envDefault:"localhost"`
-	DBPort     int    `env:"DB_PORT" envDefault:"5432"`
-	DBUser     string `env:"DB_USER" envDefault:"postgres"`
-	DBPassword string `env:"DB_PASSWORD" envDefault:"postgrespw!"`
-	DBName     string `env:"DB_NAME" envDefault:"rizzra_dev"`
-	JWTSecret  string `env:"JWT_SECRET" envDefault:"change-me-in-production"`
-	Port       int    `env:"PORT" envDefault:"8888"`
-	UploadDir  string `env:"UPLOAD_DIR" envDefault:"./uploads"`
+	DBHost     string `env:"DB_HOST"`
+	DBPort     int    `env:"DB_PORT"`
+	DBUser     string `env:"DB_USER"`
+	DBPassword string `env:"DB_PASSWORD"`
+	DBName     string `env:"DB_NAME"`
+	JWTSecret  string `env:"JWT_SECRET"`
+	Port       int    `env:"PORT"`
+	UploadDir  string `env:"UPLOAD_DIR"`
 }
 
 func (c Config) DSN() string {
-	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
+	return fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=require&channel_binding=require",
 		c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName)
 }
 
 func Load() (*Config, error) {
+	_ = godotenv.Load()
+
 	cfg := &Config{}
 	if err := env.Parse(cfg); err != nil {
 		return nil, fmt.Errorf("failed to parse config: %w", err)
